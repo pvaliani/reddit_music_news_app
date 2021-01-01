b8194141afaf51246951ff6d6bb08885950400d7
Pedram Valiani <p.valiani@gmail.com>
2021-01-01 20:42:54 +0000

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+// Import css styling, useState and useEffect
 import './App.css';
+import { useState, useEffect } from  'react';
+import StoryList from './components/StoryList';
+
+// Container for reddit stories state
 
 function App() {
+
+  // Define stories will be kept as objects
+  const [stories, setStories] = useState([])
+
+
+  // Fetch the data
+  const fetchMusicNews = () => {
+
+    // Store API url in a variable for the fetch
+    const url = `https://www.reddit.com/r/MusicNews/.json`;
+    // Fetch returns a promise object - i.e before any conversion of .json
+    fetch(url)
+    // .then returns another promise which is then converted to .json
+    .then((response) => response.json()) 
+    .then((data) => setStories(data))
+
+  };
+
+
+
   return (
-   "hello"
+   
+  "We'll return the header here later"
+
   );
 }
 
